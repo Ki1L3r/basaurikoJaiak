2e85225b7a0b70f3281e335dc85939bc28703b4c
Mikel Sanchez <lolorz@hotmail.com>
2012-08-02 20:27:30 +0200

    Google maps

--- app/events.js
+++ app/events.js
@@ -1,5 +1,57 @@
 App.Events = (function(lng, app, undefined) {
 
+		var map;
+		var initialize = function () {
+	        var mapOptions = {
+	          zoom: 6,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+	        map = new google.maps.Map(document.getElementById('map_canvas'),
+	            mapOptions);
+
+	        // Try HTML5 geolocation
+	        if(navigator.geolocation) {
+	          navigator.geolocation.getCurrentPosition(function(position) {
+	            var pos = new google.maps.LatLng(position.coords.latitude,
+	                                             position.coords.longitude);
+
+	            var infowindow = new google.maps.InfoWindow({
+	              map: map,
+	              position: pos,
+	              content: 'Estas Aquí.'
+	            });
+
+	            map.setCenter(pos);
+	          }, function() {
+	            handleNoGeolocation(true);
+	          });
+	        } else {
+	          // Browser doesn't support Geolocation
+	          handleNoGeolocation(false);
+	        }
+	      };
+
+	      var handleNoGeolocation = function (errorFlag) {
+	      	if (errorFlag) {
+	          var content = 'Error: El servicio de Geolocalizacion a fallado.';
+	        } else {
+	          var content = 'Error: Tu navegador no soporta Geolocalizacion.';
+	        }
+
+	        var options = {
+	          map: map,
+	          position: new google.maps.LatLng(43.235390, -2.891421),
+	          content: content
+	        };
+
+	        var infowindow = new google.maps.InfoWindow(options);
+	        map.setCenter(options.position);
+	      };		
+
+	lng.dom('#gps').tap(function(event) { alert("Pulsado"); });
+
+	lng.dom('#gps').tap(function(event) { initialize(); });
+
     return {
 
     }
--- app/view.js
+++ app/view.js
@@ -1,5 +1,26 @@
 App.View = (function(lng, app, undefined) {
 	
+	var twitt_template_code = "<li data-icon='user'>\n\
+            <img src='{{profile_image_url}}' />\
+            {{tweet_author}} {{tweet_date}} {{tweet_touser}}<strong>{{tweet_text}} </strong> <small> {{tweet_url}}</small> </li>";
+
+    //Create the template
+    lng.View.Template.create('twitt_template', twitt_template_code);
+
+    //  render list & pulldown-to-refresh
+    var render_list = function(tweets){
+        lng.View.Template.List.create ({
+            el : '#tweet_container',
+            template: 'tweet_template',
+            data: tweets
+        });
+
+        var markup_content = '<div id="pullDown"><span class="pullDownIcon"></span><span class="pullDownLabel">Arrastra hacia abajo para actualizar ...</span></div>';
+        lng.View.Scroll.prepend('tweet_container', markup_content);
+    
+    }
+
+
     return{
 
     }
